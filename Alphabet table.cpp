5130eb3758d4e16622fcd9bc5c86103800e1bcea
asaerxd6767 <90705181+asaerxd6767@users.noreply.github.com>
2024-12-28 14:40:25 +0200

    Alphabet Table in a Matrix way.
    Example output:
    
    A       B       C       D       E
    F       G       H       I       J
    K       L       M       N       O
    P       Q       R       S       T
    U       V       W       X       Y
    Z       #       #       #       #

--- Alphabet table.cpp
+++ Alphabet table.cpp
@@ -17,13 +17,3 @@ int main(){
     }
 return 0;
 }
-/*
-Example output:
-
-A       B       C       D       E
-F       G       H       I       J
-K       L       M       N       O
-P       Q       R       S       T
-U       V       W       X       Y
-Z       #       #       #       #
-*/
